7b233398682e1ed68f07abd255470c99cf5d3c09
Aakash Sharma <as8989607974@gmail.com>
2025-07-28 03:55:08 +0530

    updated

--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,31 +1,31 @@
-// const admin = require("firebase-admin");
-// require("dotenv").config();
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//     }),
-//   });
-// }
-
-// const db = admin.firestore();
-// module.exports = db;
-
-const { Firestore } = require('@google-cloud/firestore');
+const admin = require("firebase-admin");
 require("dotenv").config();
 
-const db = new Firestore({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  credentials: {
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  },
-  preferRest: true
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+}
 
+const db = admin.firestore();
 module.exports = db;
+
+// const { Firestore } = require('@google-cloud/firestore');
+// require("dotenv").config();
+
+// const db = new Firestore({
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   credentials: {
+//     client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//   },
+//   preferRest: true
+// });
+
+// module.exports = db;
 
 
