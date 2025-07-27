39b5d7766d717c03a2c1f9dd809a255e88d9fff3
Aakash Sharma <as8989607974@gmail.com>
2025-07-28 02:52:38 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const registerRoute = require('./routes/register');
+const registerRoute = require('./api/register');
 
 dotenv.config();
 
